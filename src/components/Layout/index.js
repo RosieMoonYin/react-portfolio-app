2255793b6f5fff3659b3dfa27d668ed89f05b39f
Rosie Burnett <rosie.victoria.burnett@gmail.com>
2023-05-09 21:21:36 +0200

    sidebar styling
    css for navbar added. incl: hover effects

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,12 @@
 import './index.scss';
+import Sidebar from '../Sidebar';
 
-const Layout = () => {
-    return <>Hello</>
+const Layout = ( ) => {
+    return ( 
+    <>
+       <Sidebar /> 
+    </>
+ )
 }
 
 export default Layout;
