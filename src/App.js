3e1e7b13163684ab8531804907ef60d420de77cf
Rosie Burnett <rosie.victoria.burnett@gmail.com>
2023-05-10 13:56:07 +0200

    updates to routing- now Sidebar is visible!
    added lots of styling (temporary logo and colour scheme)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-
+import { Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout'
 import './App.scss'
-import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout/'
 
 function App() {
   return (
-<>
-<Routes>
-  <Route path="/" element={<Layout />} />
-  
-</Routes>
-</>
-  );
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+         
+        </Route>
+      </Routes>
+    </>
+  )
 }
 
 export default App;
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,18 +1,81 @@
-import { Link } from 'react-router-dom';
-import './index.scss';
-import LogoS from '../../assets/images/logo-s.png' ;
-import LogoSubtitle from '../../assets/images/logo_sub.png';
+import './index.scss'
+import { useState } from 'react'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { Link, NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
-<div className='nav-bar'>
-    <>Hello from sidebar!!!!!</>
-    <Link className='logo' to='/'>
-        <img src={LogoS} alt='logo'/>
-        <img src={LogoSubtitle} alt='logoword'/> 
-    </Link>
+  const [showNav, setShowNav] = useState(false);
 
-</div>
+  return (
+    <div className="nav-bar">
+      <Link 
+        className="logo"
+        to="/"
+        onClick={() => setShowNav(false)}>
+        <img src={LogoS} alt="Logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+      </Link>
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <NavLink 
+          exact="true"
+          activeclassname="active"
+          to="/"
+          onClick={() => setShowNav(false)}>
+        </NavLink>
+        <NavLink 
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+          onClick={() => setShowNav(false)}>
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="portfolio-link"
+          to="/portfolio"
+          onClick={() => setShowNav(false)}
+        >
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+          onClick={() => setShowNav(false)}
+        >
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/slobodan-gaji%C4%87-006bb8b8/"
+            target="_blank"
+            rel="noreferrer"
+          >
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/bobangajicsm"
+            target="_blank"
+            rel="noreferrer"
+          >
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.youtube.com/channel/UCBu5ulO4d-d47lAVybpRTkw"
+            rel="noreferrer"
+            target="_blank"
+          >
+          </a>
+        </li>
+        <li>
+          <a href="skype:live:bobangajicsm" rel="noreferrer" target="_blank">
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
-
-export default Sidebar;
+export default Sidebar
