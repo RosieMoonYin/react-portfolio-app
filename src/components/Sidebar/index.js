b64109305d00a23151a41f006c8eb35635937097
Rosie Burnett <rosie.victoria.burnett@gmail.com>
2023-05-17 14:20:02 +0200

    fa nav icons added
    nav icons

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,6 +2,9 @@ import './index.scss'
 import { useState } from 'react'
 import LogoBlossom from '../../assets/images/pxl-blossom.png'
 import { Link, NavLink } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faHome, faUser, faEnvelope, faLinkedin, faGithub} from '@fortawesome/free-solid-svg-icons'
+
 
 const Sidebar = () => {
   const [showNav, setShowNav] = useState(false);
@@ -14,49 +17,55 @@ const Sidebar = () => {
         onClick={() => setShowNav(false)}>
         <img src={LogoBlossom} alt="Logo" />
       </Link>
+
       <nav className={showNav ? 'mobile-show' : ''}>
         <NavLink 
           exact="true"
           activeclassname="active"
           to="/"
           onClick={() => setShowNav(false)}>
+            <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
         </NavLink>
         <NavLink 
           activeclassname="active"
           className="about-link"
           to="/about"
           onClick={() => setShowNav(false)}>
+            <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
         </NavLink>
         <NavLink
           activeclassname="active"
           className="portfolio-link"
           to="/portfolio"
-          onClick={() => setShowNav(false)}
-        >
+          onClick={() => setShowNav(false)}>
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
         </NavLink>
         <NavLink
           activeclassname="active"
           className="contact-link"
           to="/contact"
-          onClick={() => setShowNav(false)}
-        >
+          onClick={() => setShowNav(false)}>
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
+        
         </NavLink>
       </nav>
       <ul>
         <li>
           <a
-            href="https://www.linkedin.com/in/slobodan-gaji%C4%87-006bb8b8/"
+            href="https://www.linkedin.com/in/rosie-victoria-burnett-7806a448/"
             target="_blank"
             rel="noreferrer"
           >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"/>
           </a>
         </li>
         <li>
           <a
-            href="https://github.com/bobangajicsm"
+            href="https://github.com/RosieMoonYin"
             target="_blank"
             rel="noreferrer"
           >
+             <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
           </a>
         </li>
         <li>
