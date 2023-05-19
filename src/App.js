69d58b88ea5d5ba9152985fb6f27d6a01c04ce6d
Rosie Burnett <rosie.victoria.burnett@gmail.com>
2023-05-19 15:43:16 +0200

    changes to home.js
    -home.js and routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
+import Home from './components/Home'
 import './App.scss'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />}>
-         
-        </Route>
+        <Route path="/" element={<Layout />}/>
+         <Route index element={<Home />}/>
       </Routes>
     </>
-  )
-}
+  );
+};
 
 export default App;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,5 @@
 import './index.scss';
+import './contact';
 
 const Home = ()=> {
 
