ccf23b934a08c045232331d17d532b9a5f5e5c21
Rosie Burnett <rosie.victoria.burnett@gmail.com>
2023-05-19 16:09:23 +0200

    adding AnimatedLetter component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,21 +1,72 @@
 import './index.scss';
-import './contact';
+import {Link} from 'react-router-dom';
+import {useEffect, useState} from "react";
+import Loader from "react-loaders";
+import Logo from "../../assets/images/pxl-blossom.png";
+import AnimatedLetters from "../AnimatedLetters";
+
 
 const Home = ()=> {
+    const [letterClass, setLetterClass] = useState('text-animate')
 
-    return (
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>Hi,<br/> I'm
-                Rosie Burnett <br/>
-                web developer
-                </h1>
-                <h2>
-                Frontend developer / Javascript / REACT
-                </h2>
-                <Link to="/contact" className="flat-button"> CONTACT ME</Link>
-            </div>
+  const nameArray = ['R', 'o', 's', 'i', 'e', 'a', 'n']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>Front End Developer / JavaScript Expert / Youtuber</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
         </div>
+        <Logo />
+      </div>
+
+      <Loader type="pacman" />
+    </>
+
+
     );
 
 };
