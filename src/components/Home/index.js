0ddb779c7ee629c22a7e0c6a6dc4fa1f470757e1
Rosie Burnett <rosie.victoria.burnett@gmail.com>
2023-05-30 11:55:12 +0200

    added Array for job
    Array for "job" on home.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -20,7 +20,11 @@ const Home = ()=> {
         <div className="text-zone">
           <h1>Hi,<br/>I'm <AnimatedLetters letterClass={letterClass}
           strArray={nameArray} idx={15}/> 
-          <br/>creative web developer</h1>
+          <br/>
+
+          <AnimatedLetters letterClass={letterClass}
+          strArray={jobArray} idx={15}/></h1>
+
           <h2>Front End Developer / JavaScript / React </h2>
           <Link to="/contact" className="flat-button">
             CONTACT ME
