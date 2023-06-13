34d7cc7fb4dfdaa461403b919a767dca37a40c64
Rosie Burnett <rosie.victoria.burnett@gmail.com>
2023-06-13 12:21:03 +0200

    integrated emailJS
    contact form with emailJS API key

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,15 +1,39 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import Sidebar from '../Sidebar';
 import './index.scss'
 
+
 const Contact = () => {
-    return (
-        <>
-            <Sidebar/>
-            <div>
-                CONTACT FORM HERE
-            </div>
-        </>
-    ) 
+    
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_dhzov9a', 'template_lx783t8', form.current, 'BH1p0pr5RpsHiWLBq')
+      .then((result) => {
+          console.log(result.text);
+          console.log("message sent")
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+  
+  return (
+    <>
+        <Sidebar/>
+        <form ref={form} onSubmit={sendEmail}>
+            <label>Name</label>
+            <input type="text" name="user_name" />
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <label>Message</label>
+            <textarea name="message" />
+            <input type="submit" value="Send" />
+        </form>
+    </>
+  );
 };
 
 export default Contact;
